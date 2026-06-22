b6c505b223d0cf08bebc7c2dfd45de0d7f2dc86c
LiorBuch <lior.buchmeier@gmail.com>
2026-06-22 12:30:11 +0300

    feat: stage b

--- pipeline_components/viewer.py
+++ pipeline_components/viewer.py
@@ -1,5 +1,7 @@
+import signal
+import sys
 import cv2
-from multiprocessing import Process,Queue
+from multiprocessing import Event, Process,Queue, queues
 import time
 
 POISON_PILL = "EOF"
@@ -8,6 +10,7 @@ class Viewer(Process):
     def __init__(self, in_queue: Queue):
         super().__init__()
         self.in_queue = in_queue
+        
 
     def run(self):
         cv2.namedWindow("Pipeline Output", cv2.WINDOW_NORMAL)
@@ -23,6 +26,22 @@ class Viewer(Process):
             # drawing bounding boxes.
             for (x, y, w, h) in detections:
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            
+            # blur stage.
+            height, width, _ = frame.shape
+            
+            for (x, y, w, h) in detections:
+                # out of bounds guard.
+                x1, y1 = max(0, x), max(0, y)
+                x2, y2 = min(width, x + w), min(height, y + h)
+                
+                if x2 > x1 and y2 > y1:
+                    roi = frame[y1:y2, x1:x2]
+                    
+                    # we use cv2.blur
+                    blurred_roi = cv2.blur(roi, (45, 45))
+                    
+                    frame[y1:y2, x1:x2] = blurred_roi
                 
             # render current Time.
             current_time = time.strftime("%H:%M:%S", time.localtime())
